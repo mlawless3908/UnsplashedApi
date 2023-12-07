d6a37cf1819e5e8b544a7febb7cc47c6b5fe2b02
zshawareb <zshawareb1906@students.mc3.edu>
2023-12-07 11:21:07 -0500

    Added query variable
    created the query variable to save input from the text box

--- UnsplashedController.java
+++ UnsplashedController.java
@@ -1,7 +1,11 @@
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
 
 public class UnsplashedController {
 
@@ -16,5 +20,12 @@ public class UnsplashedController {
 
     @FXML
     private TextArea userQuery;
+    private String query;
+
+    @FXML
+    void searchClick(ActionEvent event) {
+            query = userQuery.getText();
+            System.out.println(query);
+    }
 
 }
