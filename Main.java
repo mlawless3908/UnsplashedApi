c2621c14bacaa44dd22d0f5cab6fcdb14578f983
zshawareb <zshawareb1906@students.mc3.edu>
2023-12-08 10:11:03 -0500

    Images
    I got images to upload on clicking of the search button. Right now, I have manually added an image, but we can change the variable imageURL to be based on the parsing of data from the API. It's just a start.

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 
--- UnsplashedController.java
+++ UnsplashedController.java
@@ -3,7 +3,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.image.Image;
 import javax.swing.*;
 import java.awt.event.MouseEvent;
 
@@ -11,6 +11,9 @@ public class UnsplashedController {
 
     @FXML
     private ImageView imageOne;
+
+    @FXML
+    private ImageView imageTwo;
 
     @FXML
     private Button refreshButton;
@@ -21,11 +24,19 @@ public class UnsplashedController {
     @FXML
     private TextArea userQuery;
     private String query;
+    private String imageURL = "https://images.unsplash.com/photo-1682686580950-960d1d513532?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
 
     @FXML
     void searchClick(ActionEvent event) {
             query = userQuery.getText();
             System.out.println(query);
+            Image image1 = new Image(imageURL);
+            imageOne.setImage(image1);
+            imageTwo.setImage(image1);
+            userQuery.clear();
     }
+
+
 
 }
