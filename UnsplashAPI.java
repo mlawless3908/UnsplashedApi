76ed146dcf3c7bcdecba41ad7b71d0d337bbc632
mlawless3908 <151539348+mlawless3908@users.noreply.github.com>
2023-12-08 23:10:36 -0500

    Update UnsplashAPI.java

--- UnsplashAPI.java
+++ UnsplashAPI.java
@@ -2,4 +2,21 @@ import java.util.List;
 
 public class UnsplashAPI { 
 
-} 
+   private String accessKey; 
+   
+   /**
+   * Creates an UnsplashAPI instance with access key.
+   * @param accessKey      the key to authenticate API 
+   */
+   public UnsplashAPI(String accessKey){ 
+      this.accessKey = accessKey; 
+   }
+   
+   /**
+   * Search for images based on query
+   * @return               List of images based on query
+   */
+   public List<Image> searchImages(String moodDescription) { 
+      return null;
+   } 
+}
