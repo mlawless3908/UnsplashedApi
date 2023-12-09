ce29eb7e28a8f7616b87e0123b25a71cd197aa0f
mlawless3908 <151539348+mlawless3908@users.noreply.github.com>
2023-12-08 23:28:39 -0500

    Update Main.java
    Added: 
    
    -Access Key 
    -UnsplashAPI 
    -Create a Moodboard

--- Main.java
+++ Main.java
@@ -16,10 +16,18 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception
     {
+        //added access Key //
+        String accessKey = "g7nMae5ks7o2O1w9TIvHIkmdA5jArZ1S8fHlfutLaZo"; 
+        
+        //Morgaine added UnsplashAPI with access key & Create Moodboard instance using the initialized UnsplashAPI // 
+        UnsplashAPI unsplashAPI = new UnsplashAPI(accessKey); 
+        Moodboard moodboard = new Moodboard(unsplashAPI); 
+        
         Parent root = FXMLLoader.load(getClass().getResource("FileView.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Mood Board - Unsplashed API");
         stage.setScene(scene);
         stage.show();
+        
     }
 }
