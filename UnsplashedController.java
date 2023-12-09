8661a800b76f96e810c1641da63dabc72b230641
mlawless3908 <151539348+mlawless3908@users.noreply.github.com>
2023-12-08 23:37:00 -0500

    Update UnsplashedController.java
    -Updated program to include Moodboard, access key, an initialize controller, and update for interface

--- UnsplashedController.java
+++ UnsplashedController.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 import javax.swing.*;
 import java.awt.event.MouseEvent;
 
-public class UnsplashedController {
+public class UnsplashedController implements MoodboardObserver {
 
     @FXML
     private ImageView imageOne;
@@ -23,10 +23,24 @@ public class UnsplashedController {
 
     @FXML
     private TextArea userQuery;
+    
+    //added Moodboard 
+    private Moodboard moodboard;
+    
     private String query;
+    
+    //added Access Key
+    private String accessKey = "g7nMae5ks7o2O1w9TIvHIkmdA5jArZ1S8fHlfutLaZo"; 
+    
     private String imageURL = "https://images.unsplash.com/photo-1682686580950-960d1d513532?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-
+    //added initialize controller for unsplashAPI & observer
+    public void initialize(){
+      UnsplashAPI  unsplashAPI = new UnsplashAPI(accessKey);
+      moodboard = new Moodboard(unsplashAPI); 
+      moodboard.addObserver(this);
+   }
+      
     @FXML
     void searchClick(ActionEvent event) {
             query = userQuery.getText();
@@ -36,7 +50,13 @@ public class UnsplashedController {
             imageTwo.setImage(image1);
             userQuery.clear();
     }
-
-
+    
+    //updates the interface
+    @Override
+    public void update(Moodboard moodboard) { 
+      Image image1 = new Image(imageURL); 
+      imageOne.setImage(image1); 
+      imageTwo.setImage(image1); 
+    } 
 
 }
